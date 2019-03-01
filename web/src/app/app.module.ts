793dac89cfb8f486143d3d6c78ec65ff79fa4e35
Charlie Boggus <charlie.boggus@gmail.com>
2019-02-28 19:18:33 -0500

    Added home component to Angular frontend & bootstrap styling

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'index', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
