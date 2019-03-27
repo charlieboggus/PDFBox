8ddc3775da616023ab183fdf2278c56038973a5d
Charlie Boggus <charlie.boggus@gmail.com>
2019-03-27 13:14:15 -0400

    Added an error alert to authentication guard

--- web/src/app/shared/guards/auth.guard.ts
+++ web/src/app/shared/guards/auth.guard.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { AuthenticationService } from '../services/authentication.service';
+import { AlertService } from '../services/alert.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
 
-  constructor(private router: Router, private auth: AuthenticationService) { }
+  constructor(private router: Router, private auth: AuthenticationService, private alert: AlertService) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const currentUser = this.auth.currentUserValue;
@@ -16,6 +17,7 @@ export class AuthenticationGuard implements CanActivate {
       return true;
     }
 
+    this.alert.error('You need to be logged in to access that page.', true);
     this.router.navigate(['login'], { queryParams: { returnUrl: state.url } });
     return false;
   }
