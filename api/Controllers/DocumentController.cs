d0ccdadbf187c3a1bec93c12df4ed7ab08087c2f
Charlie Boggus <charlie.boggus@gmail.com>
2019-03-28 19:34:07 -0400

    Continued work on DocumentController class

--- api/Controllers/DocumentController.cs
+++ api/Controllers/DocumentController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PDFBox.Api.Data;
 using PDFBox.Api.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace PDFBox.Api.Controllers
 {
@@ -94,11 +95,10 @@ namespace PDFBox.Api.Controllers
 
         // HTTP POST: /api/documents/upload
         // <summary>
-        //  API method for uploading and storing a new document in database. Any documents that are able to be
-        //  converted to PDF will be converted before being stored.
+        //  API method for uploading and storing a new document in database.
         // </summary>
         [HttpPost("upload")]
-        public async Task< IActionResult > UploadDocument()
+        public async Task< IActionResult > UploadDocuments()
         {
             // Find currently authorized user in database since you need to be logged into an account to upload documents
             var authId = Int32.Parse(HttpContext.User.Identity.Name);
@@ -106,86 +106,21 @@ namespace PDFBox.Api.Controllers
             if (user == null || user.UserId != authId)
                 return Unauthorized(new { message = "You are not authorized to perform that action." });
 
-            try
-            {
-                // Create the temporary directory for creating files if it doesn't exist
-                if (!Directory.Exists("~temp"))
-                    Directory.CreateDirectory("~temp");
-                
-                // Save each file from the HTTP Request Form to the temporary directory
-                foreach (var file in Request.Form.Files)
-                {
-                    var filepath = Path.Combine("~temp", file.FileName);
-                    using (var stream = new FileStream(filepath, FileMode.Create))
-                    {
-                        await file.CopyToAsync(stream);
-                    }
-                }
+            // Get the uploaded files and convert flag from the HTTP Request Form
+            var files = Request.Form.Files;
+            var convert = Request.Form["convert"] == "true";
 
-                // Get all the files in the temporary directory & iterate over each of them to try and convert them to PDF
-                var dir = new DirectoryInfo("~temp");
-                var files = dir.GetFiles();
-                foreach (var file in files)
-                {
-                    // If the file extension is a Word extension use the convert word method
-                    if (file.Extension == ".doc" || file.Extension == ".docx")
-                        ConvertWordToPdf(file);
-                }
-
-                // Refresh the directory and get all the files in it again so we can store them in database
-                dir.Refresh();
-                files = dir.GetFiles();
-                foreach (var file in files)
-                {
-                    using (var stream = file.OpenRead())
-                    {
-                        using (var br = new BinaryReader(stream))
-                        {
-                            // Get file info
-                            var filename = file.Name;
-                            var extension = file.Extension;
-                            var size = file.Length;
-                            var data = br.ReadBytes((Int32) stream.Length);
-
-                            // Create a new Document object from file info
-                            var doc = new Document
-                            {
-                                Name = filename,
-                                Extension = extension,
-                                Size = size,
-                                CreationDate = DateTime.Now,
-                                Data = data,
-                                OwnerId = user.UserId,
-                                Owner = user
-                            };
-
-                            // Store the new document object in database
-                            await db.Documents.AddAsync(doc);
-                        }
-                    }
-                }
-
-                // Delete the temporary directory since we don't need it anymore
-                foreach (var file in files)
-                {
-                    System.IO.File.Delete(file.FullName);
-                }
-                Directory.Delete(dir.FullName);
-
-                // Finally save database changes
-                await db.SaveChangesAsync();
-
-                return Ok(new { message = "Successfully uploaded files." });
-            }
-            catch (Exception e)
-            {
-                return BadRequest(new { message = "Document upload failed: " + e.Message });
-            }
+            // If the uploaded documents should be converted to PDF call the UploadConvert method,
+            // otherwise call the UploadNoConvert method
+            if (convert)
+                return await UploadConvert(user, files);
+            else
+                return await UploadNoConvert(user, files);
         }
 
         // HTTP POST: /api/documents/convert
         // <summary>
-        //  API method for converting a document to PDF without uploading it to the database
+        //  API method for converting a document to PDF without uploading it to the database. A user account is not needed for this.
         // </summary>
         [AllowAnonymous]
         [HttpPost("convert")]
@@ -245,6 +180,142 @@ namespace PDFBox.Api.Controllers
         }
 
         // <summary>
+        //  Method to upload documents to database and convert them to PDF.
+        // </summary>
+        private async Task< IActionResult > UploadConvert(User user, IFormFileCollection files)
+        {
+            try
+            {
+                // Create the temporary directory for creating files if it doesn't exist
+                if (!Directory.Exists("~temp"))
+                    Directory.CreateDirectory("~temp");
+                
+                // Save each file from the HTTP Request Form to the temporary directory
+                foreach (var file in files)
+                {
+                    var filepath = Path.Combine("~temp", file.FileName);
+                    using (var stream = new FileStream(filepath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+
+                // Get all the files in the temporary directory & iterate over each of them to try and convert them to PDF
+                var dir = new DirectoryInfo("~temp");
+                var localFiles = dir.GetFiles();
+                foreach (var file in localFiles)
+                {
+                    // If the file extension is a Word extension use the convert Word method
+                    if (file.Extension == ".doc" || file.Extension == ".docx")
+                        ConvertWordToPdf(file);
+
+                    // If the file extension is a PPT extension use the convert PPT method
+                    if (file.Extension == ".ppt" || file.Extension == ".pptx")
+                        ConvertPowerPointToPdf(file);
+                    
+                    // If the file extension is an Excel extension use the convert Excel method
+                    if (file.Extension == ".xls" || file.Extension == ".xlsx")
+                        ConvertExcelToPdf(file);
+
+                    // No other file types are supported for conversion to PDF at this time...
+                }
+
+                // Refresh the directory and get all the files in it again so we can store them in database
+                dir.Refresh();
+                localFiles = dir.GetFiles();
+                foreach (var file in localFiles)
+                {
+                    using (var stream = file.OpenRead())
+                    {
+                        using (var br = new BinaryReader(stream))
+                        {
+                            // Get file info
+                            var filename = file.Name;
+                            var extension = file.Extension;
+                            var size = file.Length;
+                            var data = br.ReadBytes((Int32) stream.Length);
+
+                            // Create a new Document object from file info
+                            var doc = new Document
+                            {
+                                Name = filename,
+                                Extension = extension,
+                                Size = size,
+                                CreationDate = DateTime.Now,
+                                Data = data,
+                                OwnerId = user.UserId,
+                                Owner = user
+                            };
+
+                            // Store the new document object in database
+                            await db.Documents.AddAsync(doc);
+                        }
+                    }
+                }
+
+                // Delete the temporary directory since we don't need it anymore
+                foreach (var file in localFiles)
+                {
+                    System.IO.File.Delete(file.FullName);
+                }
+                Directory.Delete(dir.FullName);
+
+                // Finally save database changes
+                await db.SaveChangesAsync();
+
+                return Ok(new { message = "Successfully uploaded files." });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { message = "Unable to upload documents: " + e.Message });
+            }
+        }
+
+        // <summary>
+        //  Method to upload given documents to the database without converting them to PDF
+        // </summary>
+        private async Task< IActionResult > UploadNoConvert(User user, IFormFileCollection files)
+        {
+            try
+            {
+                foreach (var file in files)
+                {
+                    using (var stream = file.OpenReadStream())
+                    {
+                        using (var br = new BinaryReader(stream))
+                        {
+                            var filename = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                            var extension = filename.Substring(filename.LastIndexOf('.')).ToLower();
+                            var data = br.ReadBytes((Int32) stream.Length);
+                            var size = data.Length;
+
+                            var doc = new Document
+                            {
+                                Name = filename,
+                                Extension = extension,
+                                Size = size,
+                                Data = data,
+                                CreationDate = DateTime.Now,
+                                OwnerId = user.UserId,
+                                Owner = user
+                            };
+
+                            await db.Documents.AddAsync(doc);
+                        }
+                    }
+                }
+
+                await db.SaveChangesAsync();
+
+                return Ok(new { message = "Successfully uploaded files." });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { message = "Unable to convert documents: " + e.Message });
+            }
+        }
+
+        // <summary>
         //  Helper method for any method returning a download file to the client.
         //  Given a file extension, this method returns the HTML content type back to calling function.
         // </summary>
@@ -268,6 +339,10 @@ namespace PDFBox.Api.Controllers
             }
         }
 
+        // <summary>
+        //  Method to convert a given Microsoft Word document (.doc or .docx) to PDF
+        //  using the Microsoft.Office.Interop.Word library
+        // </summary>
         private void ConvertWordToPdf(FileInfo file)
         {
             object missing = System.Reflection.Missing.Value;
@@ -276,6 +351,7 @@ namespace PDFBox.Api.Controllers
             var word = new Microsoft.Office.Interop.Word.Application();
             word.Visible = false;
             word.ScreenUpdating = false;
+            word.DisplayAlerts = Microsoft.Office.Interop.Word.WdAlertLevel.wdAlertsNone;
 
             // Open the word document file
             Object filename = file.FullName;
@@ -304,6 +380,70 @@ namespace PDFBox.Api.Controllers
 
             ((Microsoft.Office.Interop.Word._Application) word).Quit(ref missing, ref missing, ref missing);
             word = null;
+
+            // Delete the original file since it's not needed anymore after conversion
+            System.IO.File.Delete(file.FullName);
+        }
+
+        // <summary>
+        //  Method to convert a given Microsoft PowerPoint presentation (.ppt or .pptx) to PDF
+        //  using the Microsoft.Office.Interop.PowerPoint library
+        // </summary>
+        private void ConvertPowerPointToPdf(FileInfo file)
+        {
+            // Initialize Interop PowerPoint Application
+            var app = new Microsoft.Office.Interop.PowerPoint.Application();
+
+            // Open the PPT file
+            var ppt = app.Presentations.Open(file.FullName, Microsoft.Office.Core.MsoTriState.msoTrue, Microsoft.Office.Core.MsoTriState.msoTrue, Microsoft.Office.Core.MsoTriState.msoFalse);
+            
+            // Export the file as PDF
+            var outfile = file.FullName.Replace(file.Extension, ".pdf");
+            ppt.SaveAs(outfile, Microsoft.Office.Interop.PowerPoint.PpSaveAsFileType.ppSaveAsPDF, Microsoft.Office.Core.MsoTriState.msoTrue);
+
+            // Close everything
+            ppt.Close();
+            ppt = null;
+
+            app.Quit();
+            app = null;
+
+            // Delete the original file since it's not needed anymore after conversion
+            System.IO.File.Delete(file.FullName);
+        }
+
+        // <summary>
+        //  Method to convert a given Microsoft Excel workbook to PDF
+        //  using the Microsoft.Office.Interop.Excel library
+        // </summary>
+        private void ConvertExcelToPdf(FileInfo file)
+        {
+            object missing = System.Reflection.Missing.Value;
+
+            // Initialize Interop Excel Application
+            var app = new Microsoft.Office.Interop.Excel.Application();
+            app.Visible = false;
+            app.ScreenUpdating = false;
+            app.DisplayAlerts = false;
+
+            // Open the Excel workbook
+            var xlsx = app.Workbooks.Open(
+                file.FullName, missing, missing, missing, 
+                missing, missing, missing, missing, 
+                missing, missing, missing, missing, 
+                missing, missing, missing
+            );
+
+            // Export the workbook as PDF
+            var outfile = file.FullName.Replace(file.Extension,  ".pdf");
+            xlsx.ExportAsFixedFormat(Microsoft.Office.Interop.Excel.XlFixedFormatType.xlTypePDF, outfile, missing, missing, missing, missing, missing, missing, missing);
+
+            // Close everything
+            xlsx.Close(Microsoft.Office.Interop.Excel.XlSaveAction.xlDoNotSaveChanges, missing, missing);
+            xlsx = null;
+
+            app.Quit();
+            app = null;
 
             // Delete the original file since it's not needed anymore after conversion
             System.IO.File.Delete(file.FullName);
