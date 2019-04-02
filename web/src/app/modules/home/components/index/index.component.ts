d20564611914cb775dcf30d73b4175e10b98c4a6
Charlie Boggus <charlie.boggus@gmail.com>
2019-04-02 13:16:27 -0400

    Working on index page and convert page of web frontend

--- web/src/app/modules/home/components/index/index.component.ts
+++ web/src/app/modules/home/components/index/index.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from 'src/app/shared/services/authentication.service';
+import { User } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  currentUser: User;
+
+  constructor(private auth: AuthenticationService, private router: Router) {
+    this.auth.currentUser.subscribe(x => this.currentUser = x);
+  }
 
   ngOnInit() {
   }
 
+  redirectConvert() {
+    this.router.navigate(['convert']);
+  }
+
+  redirectLogin() {
+    this.router.navigate(['login']);
+  }
+
+  redirectRegister() {
+    this.router.navigate(['register']);
+  }
+
+  redirectUpload() {
+    this.router.navigate(['upload']);
+  }
+
+  redirectView() {
+    this.router.navigate(['documents']);
+  }
 }
