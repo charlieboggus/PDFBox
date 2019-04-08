3c01795cd871920764201d5ad2bd195f71cc0a41
Charlie Boggus <charlie.boggus@gmail.com>
2019-04-08 15:06:58 -0400

    Created Unit Test project

---
+++ tests/DocumentTests.cs
@@ -0,0 +1,10 @@
+using System;
+using Xunit;
+
+namespace tests
+{
+    public class DocumentTests
+    {
+        // TODO: unit tests
+    }
+}
---
+++ tests/UserTests.cs
@@ -0,0 +1,10 @@
+using System;
+using Xunit;
+
+namespace tests
+{
+    public class UserTests
+    {
+        // TODO: unit tests
+    }
+}
